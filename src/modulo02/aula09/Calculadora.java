038deac7d13b29d000e12f79814f0c2d499b99a9
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-22 08:47:19 -0300

    A9 - Adicionando validação ao divisor

--- src/modulo02/aula09/Calculadora.java
+++ src/modulo02/aula09/Calculadora.java
@@ -13,6 +13,10 @@ public class Calculadora {
         numero1 = lerNumero("\nDigite o 1º número: ");
         numero2 = lerNumero("Digite o 2º número: ");
 
+        if (!validaEntrada(numero2)) {
+            numero2 = Double.parseDouble(entrada.nextLine());
+        }
+
         imprimeCabecalhoOperacoes();
         imprimeResultado(numero1, numero2);
     }
@@ -27,28 +31,33 @@ public class Calculadora {
         return numero;
     }
 
+    static boolean validaEntrada(double num2) {
+        boolean ehValido = true;
+        if (num2 == 0.0) {
+            System.out.print("\nO divisor não pode ser igual a 0, digite o 2º número novamente: ");
+            ehValido = false;
+        }
+        return ehValido;
+    }
+
     static void imprimeCabecalhoOperacoes() {
         System.out.println("\n****** RESULTADO DAS OPERAÇÕES ******");
     }
 
     static double adicao(double num1, double num2) {
-        double adicao = (num1 + num2);
-        return adicao;
+        return (num1 + num2);
     }
 
     static double subtracao(double num1, double num2) {
-        double subtracao = (num1 - num2);
-        return subtracao;
+        return (num1 - num2);
     }
 
     static double multiplicacao(double num1, double num2) {
-        double multiplicacao = (num1 * num2);
-        return multiplicacao;
+        return (num1 * num2);
     }
 
     static double divisao(double num1, double num2) {
-        double divisao = (num1 / num2);
-        return divisao;
+        return (num1 / num2);
     }
 
     static void imprimeResultado(double num1, double num2) {
