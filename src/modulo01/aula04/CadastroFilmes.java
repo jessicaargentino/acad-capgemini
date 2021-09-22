b3d8a10a0b25c9f1c454bb4e09fcbf4a335331ed
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-22 11:12:51 -0300

    A4 - Otimização do código utilizando funções

--- src/modulo01/aula04/CadastroFilmes.java
+++ src/modulo01/aula04/CadastroFilmes.java
@@ -3,57 +3,26 @@ package modulo01.aula04;
 import java.util.Scanner;
 
 public class CadastroFilmes {
+    static Scanner entrada = new Scanner(System.in);
+    static String nome;
+    static int anoLancamento;
+
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int opcao, anoLancamento;
-        String nome, descricao, genero, produtora, tema;
+        int opcao;
 
-        System.out.println("***** CADASTRO DE AUDIOVISUAIS *****");
-        System.out.println("\nOlá! Seja bem vindo(a) ao cadastro, selecione sua opção:\n");
-        System.out.println("1 - Cadastrar filme");
-        System.out.println("2 - Cadastrar série");
-        System.out.println("3 - Cadastrar documentário");
-        System.out.println("0 - Sair");
-        System.out.print("Opção selecionada: ");
-        opcao = entrada.nextInt();
-
-        entrada.nextLine();
+        exibeCabecalho();
+        exibeMenu();
+        opcao = escolheOpcao();
 
         switch (opcao) {
             case 1:
-                System.out.println("\n***** CADASTRO DE FILMES *****");
-                System.out.print("Nome: ");
-                nome = entrada.nextLine();
-                System.out.print("Descrição: ");
-                descricao = entrada.nextLine();
-                System.out.print("Gênero: ");
-                genero = entrada.nextLine();
-                System.out.print("Ano de lançamento: ");
-                anoLancamento = entrada.nextInt();
-
-                System.out.printf("\nO filme %s foi cadastrado com sucesso!\n", nome);
+                cadastraFilme();
                 break;
             case 2:
-                System.out.println("\n***** CADASTRO DE SÉRIES *****");
-                System.out.print("Nome: ");
-                nome = entrada.nextLine();
-                System.out.print("Produtora: ");
-                produtora = entrada.nextLine();
-                System.out.print("Ano de lançamento: ");
-                anoLancamento = entrada.nextInt();
-
-                System.out.printf("\nA série %s foi cadastrada com sucesso!\n", nome);
+                cadastraSerie();
                 break;
             case 3:
-                System.out.println("\n***** CADASTRO DE DOCUMENTÁRIOS *****");
-                System.out.print("Nome: ");
-                nome = entrada.nextLine();
-                System.out.print("Tema: ");
-                tema = entrada.nextLine();
-                System.out.print("Ano de lançamento: ");
-                anoLancamento = entrada.nextInt();
-
-                System.out.printf("\nO documentário %s foi cadastrado com sucesso!\n", nome);
+                cadastraDocumentario();
                 break;
             case 0:
                 System.out.println("\nObrigada por utilizar nosso cadastro!");
@@ -62,7 +31,70 @@ public class CadastroFilmes {
                 System.out.println("Opção inválida!");
                 break;
         }
+    }
 
-        entrada.close();
+    static void exibeCabecalho() {
+        System.out.println("***** CADASTRO DE AUDIOVISUAIS *****");
+        System.out.println("\nOlá! Seja bem vindo(a) ao cadastro, selecione sua opção:\n");
+    }
+
+    static void exibeMenu() {
+        System.out.println("1 - Cadastrar filme");
+        System.out.println("2 - Cadastrar série");
+        System.out.println("3 - Cadastrar documentário");
+        System.out.println("0 - Sair");
+    }
+
+    static int escolheOpcao() {
+        int opcao;
+
+        System.out.print("Opção selecionada: ");
+        opcao = entrada.nextInt();
+        entrada.nextLine();
+        return opcao;
+    }
+
+    static void cadastraFilme() {
+        String descricao, genero;
+
+        System.out.println("\n***** CADASTRO DE FILMES *****");
+        System.out.print("Nome: ");
+        nome = entrada.nextLine();
+        System.out.print("Descrição: ");
+        descricao = entrada.nextLine();
+        System.out.print("Gênero: ");
+        genero = entrada.nextLine();
+        System.out.print("Ano de lançamento: ");
+        anoLancamento = entrada.nextInt();
+
+        System.out.printf("\nO filme %s foi cadastrado com sucesso!\n", nome);
+    }
+
+    static void cadastraSerie() {
+        String produtora;
+
+        System.out.println("\n***** CADASTRO DE SÉRIES *****");
+        System.out.print("Nome: ");
+        nome = entrada.nextLine();
+        System.out.print("Produtora: ");
+        produtora = entrada.nextLine();
+        System.out.print("Ano de lançamento: ");
+        anoLancamento = entrada.nextInt();
+
+        System.out.printf("\nA série %s foi cadastrada com sucesso!\n", nome);
+    }
+
+    static void cadastraDocumentario() {
+        String tema;
+
+        System.out.println("\n***** CADASTRO DE DOCUMENTÁRIOS *****");
+        System.out.print("Nome: ");
+        nome = entrada.nextLine();
+        System.out.print("Tema: ");
+        tema = entrada.nextLine();
+        System.out.print("Ano de lançamento: ");
+        anoLancamento = entrada.nextInt();
+
+        System.out.printf("\nO documentário %s foi cadastrado com sucesso!\n", nome);
     }
 }
