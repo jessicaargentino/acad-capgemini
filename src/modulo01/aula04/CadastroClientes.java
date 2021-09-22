490b290d8f6ec880117721de968c74947a4a0de1
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-22 11:01:23 -0300

    A4 - Otimização do código

--- src/modulo01/aula04/CadastroClientes.java
+++ src/modulo01/aula04/CadastroClientes.java
@@ -3,16 +3,22 @@ package modulo01.aula04;
 import java.util.Scanner;
 
 public class CadastroClientes {
+    static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        String nome, sobrenome;
         short idade;
 
         System.out.println("***** CADASTRO DE CLIENTES *****");
-        System.out.println("\nOlá! Seja bem vindo(a) ao cadastro de clientes!\n");
+        System.out.println("Olá! Seja bem vindo(a) ao cadastro de clientes!\n");
         System.out.print("Digite sua idade: ");
         idade = entrada.nextShort();
         entrada.nextLine();
+
+        cadastraCliente(idade);
+    }
+
+    static void cadastraCliente(int idade) {
+        String nome, sobrenome;
 
         if (idade >= 18) {
             System.out.print("Digite seu nome: ");
@@ -21,13 +27,15 @@ public class CadastroClientes {
             sobrenome = entrada.nextLine();
 
             System.out.println("\nSeu cadastro foi realizado com sucesso!\n");
-            System.out.println("***** FICHA DE CADASTRO *****");
-            System.out.printf("Nome cliente: %s %s\n", nome, sobrenome);
-            System.out.printf("Idade: %d anos", idade);
+            exibeCadastro(nome, sobrenome, idade);
         } else {
             System.out.println("Menor de idade - Não foi possível realizar o cadastro!");
         }
+    }
 
-        entrada.close();
+    static void exibeCadastro(String nome, String sobrenome, int idade) {
+        System.out.println("***** FICHA DE CADASTRO *****");
+        System.out.printf("Nome cliente: %s %s\n", nome, sobrenome);
+        System.out.printf("Idade: %d anos", idade);
     }
 }
