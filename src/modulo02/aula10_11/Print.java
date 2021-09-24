6992d958d5aa071ffa207f687d6eea067ce7e416
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-24 14:31:16 -0300

    A10-11 - Revisão sobre tipos de print

---
+++ src/modulo02/aula10_11/Print.java
@@ -0,0 +1,15 @@
+package modulo02.aula10_11;
+
+public class Print{
+    public static void main(String[] args){
+        System.out.println("Jéssica");
+        System.out.print("Ola");
+        System.out.printf("\n\tO meu nome é %s e minha idade é %d.\n","Jéssica", 22);
+        System.out.print("Ola");
+
+        System.out.println("==============================");
+        System.out.println("Jéssica"+"Argentino");
+        System.out.print("Jéssica"+"Argentino");
+        System.out.printf("Jéssica%f"+"Argentino%d"+"Dias%f",10.5, 8, 9.8 );
+    }
+}
