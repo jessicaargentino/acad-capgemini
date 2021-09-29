3775bdd1ceebe054307180f8fc7b67cde007ed45
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-29 17:43:10 -0300

    A14 - Mudança dos métodos de static para default

--- src/modulo02/aula14/Calculadora.java
+++ src/modulo02/aula14/Calculadora.java
@@ -3,22 +3,22 @@ package modulo02.aula14;
 public class Calculadora {
     int numero1;
 
-    static int soma(int numero1, int numero2) {
+    int soma(int numero1, int numero2) {
         int soma = (numero1 + numero2);
         return soma;
     }
 
-    static int subtracao(int numero1, int numero2) {
+    int subtracao(int numero1, int numero2) {
         int subtracao = (numero1 - numero2);
         return subtracao;
     }
 
-    static int multiplicacao(int numero1, int numero2) {
+    int multiplicacao(int numero1, int numero2) {
         int multiplicacao = (numero1 * numero2);
         return multiplicacao;
     }
 
-    static int divisao(int numero1, int numero2) {
+    int divisao(int numero1, int numero2) {
         int divisao = (numero1 / numero2);
         return divisao;
     }
