f76285b1471a8ab3358b847eee8580b08b1f24a8
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-30 10:50:45 -0300

    A14 - Exercício de calculadora simples com POO

--- src/modulo02/aula14/Calculadora.java
+++ src/modulo02/aula14/Calculadora.java
@@ -1,23 +1,32 @@
 package modulo02.aula14;
 
 public class Calculadora {
+    // DEFINIÇÃO DO ATRIBUTO DA CLASSE
     int numero1;
 
+    // MÉTODO QUE RECEBE DOIS NÚMEROS INTEIROS E RETORNA O RESULTADO DA SOMA DOS
+    // MESMOS
     int soma(int numero1, int numero2) {
         int soma = (numero1 + numero2);
         return soma;
     }
 
+    // MÉTODO QUE RECEBE DOIS NÚMEROS INTEIROS E RETORNA O RESULTADO DA SUBTRAÇÃO
+    // DOS MESMOS
     int subtracao(int numero1, int numero2) {
         int subtracao = (numero1 - numero2);
         return subtracao;
     }
 
+    // MÉTODO QUE RECEBE DOIS NÚMEROS INTEIROS E RETORNA O RESULTADO DA
+    // MULTIPLICAÇÃO DOS MESMOS
     int multiplicacao(int numero1, int numero2) {
         int multiplicacao = (numero1 * numero2);
         return multiplicacao;
     }
 
+    // MÉTODO QUE RECEBE DOIS NÚMEROS INTEIROS E RETORNA O RESULTADO DA DIVISÃO DOS
+    // MESMOS
     int divisao(int numero1, int numero2) {
         int divisao = (numero1 / numero2);
         return divisao;
--- src/modulo02/aula14/MainCalculadora.java
+++ src/modulo02/aula14/MainCalculadora.java
@@ -2,5 +2,16 @@ package modulo02.aula14;
 
 public class MainCalculadora {
     public static void main(String[] args) {
+
+        // CRIANDO UMA INSTÂNCIA DA CLASSE CALCULADORA(OBJETO)
+        Calculadora calculadora = new Calculadora();
+
+        // CHAMADA DOS MÉTODOS DA CLASSE E ARMAZENAMENTO DOS RETORNOS EM VARIÁVEIS
+        int soma = calculadora.soma(5, 2);
+        int subtracao = calculadora.subtracao(5, 2);
+        int multiplicacao = calculadora.multiplicacao(5, 2);
+        int divisao = calculadora.divisao(5, 2);
+
     }
+
 }
