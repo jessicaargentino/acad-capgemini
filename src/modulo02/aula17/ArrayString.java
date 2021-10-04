2f370853fc68ccdc2901fe38007d915dce5f96ee
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-04 16:30:10 -0300

    A17 - Explicação sobre arrays de String

---
+++ src/modulo02/aula17/ArrayString.java
@@ -0,0 +1,23 @@
+package modulo02.aula17;
+
+public class ArrayString {
+    public static void main(String[] args) {
+        /* ARRAY DE STRINGS COM 5 COMPONENTES */
+        String nome[] = new String[5];
+
+        /* IMPRESSÃO DE TESTE PARA VER QUE O DEFAULT DE STRING É NULL */
+        System.out.println(nome[0]);
+
+        /* ATRIBUIÇÃO DE VALORES PARA AS POSIÇÕES DO ARRAY */
+        nome[0] = "Laura Palmer";
+        nome[1] = "R2D2";
+        nome[2] = "Norman Bates";
+        nome[3] = "Chiriro";
+        nome[4] = "Kaonashi";
+
+        /* PERCORRENDO OS VALORES COM O FOR E OS IMPRIMINDO */
+        for (int n = 0; n < nome.length; n++) {
+            System.out.println(nome[n]);
+        }
+    }
+}
