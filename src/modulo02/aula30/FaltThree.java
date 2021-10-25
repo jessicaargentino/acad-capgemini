9aac8c458391ec145b84dbdd4a05f574c87e22f2
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-10-25 19:47:31 -0300

    A30 - Código sobre erro em ArrayList

---
+++ src/modulo02/aula30/FaltThree.java
@@ -0,0 +1,17 @@
+package modulo02.aula30;
+
+import java.util.ArrayList;
+
+public class FaltThree {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+
+        System.out.println(numbers[6]);
+
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Maykon");
+        names.add("Jojo");
+        names.add("Ines");
+        System.out.println(names.get(5)); // ArrayIndexOutOfBoundsException
+    }
+}
