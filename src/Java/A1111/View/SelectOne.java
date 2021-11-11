062bdddaf2e0da2038963b83d57318b0e09c5ca7
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-11-11 20:10:24 -0300

    A11.11 - Conexão com Postgres e select

--- src/Java/A1111/View/SelectOne.java
+++ src/Java/A1111/View/SelectOne.java
@@ -1,25 +1,35 @@
+package Java.A1111.View;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class ConnectionTwo {
+public class SelectOne {
     public static void main(String[] args) {
         /** Conexão com banco de dados Postgres */
         try {
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres",
-                    "admin");
+            String driverType = "jdbc";
+            String driverName = "postgresql";
+            String host = "localhost";
+            int port = 5432;
+            String database = "postgres";
+            String user = "postgres";
+            String password = "admin";
+
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
+            Connection connection = DriverManager.getConnection(connectionString, user, password);
 
             /** Statement - executar query */
             Statement statement = connection.createStatement();
-            statement.execute("SELECT * FROM cidade");
+            String query = "SELECT * FROM cidade";
+            statement.execute(query);
             /** ResultSet - guardar resultado da query */
             ResultSet result = statement.getResultSet();
 
             /**
-             * Percorre os resultados, a cada entrada guarda o valor na variável e exibe na
-             * tela
+             * Percorre os resultados, a cada entrada guarda o valor nas variáveis e exibe
+             * na tela
              */
             while (result.next()) {
                 int id = result.getInt("id");
--- src/Java/A1111/View/SelectTwo.java
+++ src/Java/A1111/View/SelectTwo.java
@@ -1,33 +1,27 @@
+package Java.A1111.View;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class ConnectionOne {
+public class SelectTwo {
     public static void main(String[] args) {
         /** Conexão com banco de dados Postgres */
         try {
-            String driverType = "jdbc";
-            String driverName = "postgresql";
-            String host = "localhost";
-            int port = 5432;
-            String database = "postgres";
-            String user = "postgres";
-            String password = "admin";
-
-            String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
-            Connection connection = DriverManager.getConnection(connectionString, user, password);
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres",
+                    "admin");
 
             /** Statement - executar query */
             Statement statement = connection.createStatement();
-            statement.execute("SELECT * FROM cidade");
+            String query = "SELECT * FROM cidade";
+            statement.execute(query);
             /** ResultSet - guardar resultado da query */
             ResultSet result = statement.getResultSet();
 
             /**
-             * Percorre os resultados, a cada entrada guarda o valor nas variáveis e exibe
-             * na tela
+             * Percorre os resultados, a cada entrada guarda o valor na variável e exibe na
+             * tela
              */
             while (result.next()) {
                 int id = result.getInt("id");
